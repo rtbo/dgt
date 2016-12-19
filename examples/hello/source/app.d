8b37a0e3b7de2f486eed890476a8358aaf18b6f4
Remi THEBAULT <remi.thebault@gmail.com>
2016-12-19 13:13:28 +0100

    dfmt

--- examples/hello/source/app.d
+++ examples/hello/source/app.d
@@ -1,40 +1,39 @@
-
 import dgt.application;
 import dgt.window;
 import dgt.event;
 import key = dgt.keys;
+import dgt.vg.path;
+import dgt.vg.context;
 
 import std.typecons : scoped;
 import std.stdio;
-
 
 int main()
 {
     auto app = new Application();
     auto win = new Window();
     win.onKeyDown += (WindowKeyEvent ev) {
-        switch(ev.sym) {
-            case key.Sym.f:
-                win.showFullscreen();
-                break;
-            case key.Sym.n:
-                win.showNormal();
-                break;
-            case key.Sym.m:
-                win.showMaximized();
-                break;
-            case key.Sym.s:
-                win.showMinimized();
-                break;
-            default:
-                break;
+        switch (ev.sym)
+        {
+        case key.Sym.f:
+            win.showFullscreen();
+            break;
+        case key.Sym.n:
+            win.showNormal();
+            break;
+        case key.Sym.m:
+            win.showMaximized();
+            break;
+        case key.Sym.s:
+            win.showMinimized();
+            break;
+        default:
+            break;
         }
     };
     win.onExpose += (WindowExposeEvent ev) {
-        import dgt.vg.path;
-        import dgt.vg.context;
         auto ctx = win.createVgContext();
-        scope(exit) ctx.dispose();
+        scope (exit) ctx.dispose();
 
         ctx.lineWidth = 5f;
         auto p = new Path([10, 10]);
@@ -42,9 +41,7 @@ int main()
         p.lineTo([400, 10]);
         ctx.drawPath(p, PaintModeFlags(PaintMode.stroke));
     };
-    win.onClosed += (Window) {
-        app.exit(0);
-    };
+    win.onClosed += (Window) { app.exit(0); };
     win.show();
     return app.loop();
 }
