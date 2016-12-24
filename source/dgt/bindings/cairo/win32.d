7dede060c53f46489d13aedf0fa25469fc4929f1
Remi THEBAULT <remi.thebault@gmail.com>
2016-12-24 10:08:36 +0100

    comment unused cairo symbols
    improve compile time

--- source/dgt/bindings/cairo/win32.d
+++ source/dgt/bindings/cairo/win32.d
@@ -2,36 +2,36 @@ module dgt.bindings.cairo.win32;
 
 version (Windows):
 
-import dgt.bindings.cairo.types;
-import dgt.bindings.cairo.enums;
-import dgt.bindings;
+// import dgt.bindings.cairo.types;
+// import dgt.bindings.cairo.enums;
+// import dgt.bindings;
 
-import core.sys.windows.windef;
+// import core.sys.windows.windef;
 
-__gshared Symbol!(cairo_surface_t*, HDC) cairo_win32_surface_create;
+// __gshared Symbol!(cairo_surface_t*, HDC) cairo_win32_surface_create;
 
-__gshared Symbol!(cairo_surface_t*, HDC) cairo_win32_printing_surface_create;
+// __gshared Symbol!(cairo_surface_t*, HDC) cairo_win32_printing_surface_create;
 
-__gshared Symbol!(cairo_surface_t*, HDC, cairo_format_t, int, int) cairo_win32_surface_create_with_ddb;
+// __gshared Symbol!(cairo_surface_t*, HDC, cairo_format_t, int, int) cairo_win32_surface_create_with_ddb;
 
-__gshared Symbol!(cairo_surface_t*, cairo_format_t, int, int) cairo_win32_surface_create_with_dib;
+// __gshared Symbol!(cairo_surface_t*, cairo_format_t, int, int) cairo_win32_surface_create_with_dib;
 
-__gshared Symbol!(HDC, cairo_surface_t*) cairo_win32_surface_get_dc;
+// __gshared Symbol!(HDC, cairo_surface_t*) cairo_win32_surface_get_dc;
 
-__gshared Symbol!(cairo_surface_t*, cairo_surface_t*) cairo_win32_surface_get_image;
+// __gshared Symbol!(cairo_surface_t*, cairo_surface_t*) cairo_win32_surface_get_image;
 
-__gshared Symbol!(cairo_font_face_t*, LOGFONTW*) cairo_win32_font_face_create_for_logfontw;
+// __gshared Symbol!(cairo_font_face_t*, LOGFONTW*) cairo_win32_font_face_create_for_logfontw;
 
-__gshared Symbol!(cairo_font_face_t*, HFONT) cairo_win32_font_face_create_for_hfont;
+// __gshared Symbol!(cairo_font_face_t*, HFONT) cairo_win32_font_face_create_for_hfont;
 
-__gshared Symbol!(cairo_font_face_t*, LOGFONTW*, HFONT) cairo_win32_font_face_create_for_logfontw_hfont;
+// __gshared Symbol!(cairo_font_face_t*, LOGFONTW*, HFONT) cairo_win32_font_face_create_for_logfontw_hfont;
 
-__gshared Symbol!(cairo_status_t, cairo_scaled_font_t*, HDC) cairo_win32_scaled_font_select_font;
+// __gshared Symbol!(cairo_status_t, cairo_scaled_font_t*, HDC) cairo_win32_scaled_font_select_font;
 
-__gshared Symbol!(void, cairo_scaled_font_t*) cairo_win32_scaled_font_done_font;
+// __gshared Symbol!(void, cairo_scaled_font_t*) cairo_win32_scaled_font_done_font;
 
-__gshared Symbol!(double, cairo_scaled_font_t*) cairo_win32_scaled_font_get_metrics_factor;
+// __gshared Symbol!(double, cairo_scaled_font_t*) cairo_win32_scaled_font_get_metrics_factor;
 
-__gshared Symbol!(void, cairo_scaled_font_t*, cairo_matrix_t*) cairo_win32_scaled_font_get_logical_to_device;
+// __gshared Symbol!(void, cairo_scaled_font_t*, cairo_matrix_t*) cairo_win32_scaled_font_get_logical_to_device;
 
-__gshared Symbol!(void, cairo_scaled_font_t*, cairo_matrix_t*) cairo_win32_scaled_font_get_device_to_logical;
+// __gshared Symbol!(void, cairo_scaled_font_t*, cairo_matrix_t*) cairo_win32_scaled_font_get_device_to_logical;
--- source/dgt/bindings/cairo/xcb.d
+++ source/dgt/bindings/cairo/xcb.d
@@ -11,21 +11,21 @@ import xcb.render;
 
 __gshared Symbol!(cairo_surface_t*, xcb_connection_t*, xcb_drawable_t, xcb_visualtype_t*, int, int) cairo_xcb_surface_create;
 
-__gshared Symbol!(cairo_surface_t*, xcb_connection_t*, xcb_screen_t*, xcb_pixmap_t, int, int) cairo_xcb_surface_create_for_bitmap;
+// __gshared Symbol!(cairo_surface_t*, xcb_connection_t*, xcb_screen_t*, xcb_pixmap_t, int, int) cairo_xcb_surface_create_for_bitmap;
 
-__gshared Symbol!(cairo_surface_t*, xcb_connection_t*, xcb_screen_t*, xcb_drawable_t,
-        xcb_render_pictforminfo_t*, int, int) cairo_xcb_surface_create_with_xrender_format;
+// __gshared Symbol!(cairo_surface_t*, xcb_connection_t*, xcb_screen_t*, xcb_drawable_t,
+//         xcb_render_pictforminfo_t*, int, int) cairo_xcb_surface_create_with_xrender_format;
 
 __gshared Symbol!(void, cairo_surface_t*, int, int) cairo_xcb_surface_set_size;
 
-__gshared Symbol!(void, cairo_surface_t*, xcb_drawable_t, int, int) cairo_xcb_surface_set_drawable;
+// __gshared Symbol!(void, cairo_surface_t*, xcb_drawable_t, int, int) cairo_xcb_surface_set_drawable;
 
-__gshared Symbol!(xcb_connection_t*, cairo_device_t*) cairo_xcb_device_get_connection;
+// __gshared Symbol!(xcb_connection_t*, cairo_device_t*) cairo_xcb_device_get_connection;
 
-__gshared Symbol!(void, cairo_device_t*, int, int) cairo_xcb_device_debug_cap_xshm_version;
+// __gshared Symbol!(void, cairo_device_t*, int, int) cairo_xcb_device_debug_cap_xshm_version;
 
-__gshared Symbol!(void, cairo_device_t*, int, int) cairo_xcb_device_debug_cap_xrender_version;
+// __gshared Symbol!(void, cairo_device_t*, int, int) cairo_xcb_device_debug_cap_xrender_version;
 
-__gshared Symbol!(void, cairo_device_t*, int) cairo_xcb_device_debug_set_precision;
+// __gshared Symbol!(void, cairo_device_t*, int) cairo_xcb_device_debug_set_precision;
 
-__gshared Symbol!(int, cairo_device_t*) cairo_xcb_device_debug_get_precision;
+// __gshared Symbol!(int, cairo_device_t*) cairo_xcb_device_debug_get_precision;
