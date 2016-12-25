c4c61483220034af9f79dc7a20c106f2b6a314a4
Remi THEBAULT <remi.thebault@gmail.com>
2016-12-25 19:37:42 +0100

    FontCache.fontRequest and result

--- source/dgt/text/fontcache.d
+++ source/dgt/text/fontcache.d
@@ -5,6 +5,90 @@ import dgt.rc;
 
 import std.exception;
 import std.string;
+import std.experimental.logger;
+
+/// Font size (expressed in pts or px)
+struct FontSize
+{
+    /// Unit of the size value
+    enum Unit
+    {
+        pts,
+        px
+    }
+
+    Unit unit;
+    double value;
+
+    /// Returns a FontSize with value expressed in pts
+    static FontSize pts(double pts)
+    {
+        return FontSize(Unit.pts, pts);
+    }
+    /// Returns a FontSize with value expressed in px
+    static FontSize px(double px)
+    {
+        return FontSize(Unit.px, px);
+    }
+}
+
+/// Font style as defined by the CSS specification
+enum FontStyle
+{
+    normal,
+    italic,
+    oblique,
+}
+
+/// Font weight as defined by the CSS specification.
+/// An integer is typically expected for the weight. Either a value of this enum
+/// or a number between 100 and 900 can be provided in place.
+enum FontWeight : int
+{
+    normal = 400,
+    bold = 700,
+}
+
+/// Font variant as defined by the CSS specification.
+/// Unsupported at the moment.
+enum FontVariant
+{
+    normal,
+    smallCaps,
+}
+
+/// A structured font request.
+/// Although taking parameters as defined by the CSS specification,
+/// no guarantee is made that the font matching will be 100% CSS compliant.
+struct FontRequest
+{
+    string family;
+    FontSize size = FontSize.pts(10);
+    FontStyle style = FontStyle.normal;
+    int weight = FontWeight.normal;
+    FontVariant variant = FontVariant.normal;
+    string foundry;
+}
+
+
+/// Result of a font request. A request typically returns a range of those results.
+/// The first element will be the best match, the next ones fallbacks.
+/// Has same fields as the font request, so that application can check what was
+/// really matched.
+/// Also has filename and face index within the file.
+struct FontResult
+{
+    string family;
+    FontSize size = FontSize.pts(10);
+    FontStyle style = FontStyle.normal;
+    int weight = FontWeight.normal;
+    FontVariant variant = FontVariant.normal;
+    string foundry;
+
+    string filename;
+    int faceIndex;
+}
+
 
 private __gshared FontCache instance_;
 
@@ -67,7 +151,124 @@ class FontCache : Disposable
 
     void addAppFontFile(string file)
     {
+        FcConfigAppFontClear(config_);
         enforce(FcConfigAppFontAddFile(config_, toStringz(file)));
         appFontFiles_ ~= file;
+    }
+
+    FontResult[] requestFont(in FontRequest req)
+    {
+        auto pat = FcPatternCreate();
+        scope(exit)
+            FcPatternDestroy(pat);
+        FcPatternAddString(pat, FC_FAMILY, toStringz(req.family));
+        FcPatternAddInteger(pat, FC_SLANT, styleToFcSlant(req.style));
+        FcPatternAddInteger(pat, FC_WEIGHT, FcWeightFromOpenType(req.weight));
+        // FIXME: get dpi from Screen
+        FcPatternAddDouble(pat, FC_DPI, 96.0);
+        if (req.size.unit == FontSize.Unit.px)
+        {
+            FcPatternAddDouble(pat, FC_PIXEL_SIZE, req.size.value);
+        }
+        else
+        {
+            FcPatternAddDouble(pat, FC_SIZE, req.size.value);
+        }
+        if (req.foundry.length)
+        {
+            FcPatternAddString(pat, FC_FOUNDRY, toStringz(req.foundry));
+        }
+
+        FcConfigSubstitute(config_, pat, FcMatchPattern);
+        FcDefaultSubstitute(pat);
+
+        FcResult dummy;
+        auto patterns = FcFontSort(config_, pat, FcTrue, null, &dummy);
+        if (!patterns)
+        {
+            errorf("could not match any font.");
+            return [];
+        }
+        scope(exit)
+            FcFontSetDestroy(patterns);
+
+        FontResult[] res;
+        foreach (i; 0 .. patterns.nfont)
+        {
+            auto p = FcFontRenderPrepare(config_, pat, patterns.fonts[i]);
+            if (p)
+            {
+                scope(exit)
+                    FcPatternDestroy(p);
+                double dval = void;
+                char* sval = void;
+                int ival = void;
+                FontResult fr;
+                if (FcPatternGetString(p, FC_FAMILY, 0, &sval) == FcResultMatch)
+                {
+                    fr.family = fromStringz(sval).idup;
+                }
+                if (FcPatternGetInteger(p, FC_SLANT, 0, &ival) == FcResultMatch)
+                {
+                    fr.style = fcSlantToStyle(ival);
+                }
+                if (FcPatternGetInteger(p, FC_WEIGHT, 0, &ival) == FcResultMatch)
+                {
+                    fr.weight = FcWeightToOpenType(ival);
+                }
+                immutable key = req.size.unit == FontSize.Unit.pts ? FC_SIZE : FC_PIXEL_SIZE;
+                if (FcPatternGetDouble(p, toStringz(key), 0, &dval) == FcResultMatch)
+                {
+                    fr.size = req.size.unit == FontSize.Unit.pts ?
+                        FontSize.pts(dval) : FontSize.px(dval);
+                }
+                if (FcPatternGetString(p, FC_FOUNDRY, 0, &sval) == FcResultMatch)
+                {
+                    fr.foundry = fromStringz(sval).idup;
+                }
+                if (FcPatternGetString(p, FC_FILE, 0, &sval) == FcResultMatch)
+                {
+                    fr.filename = fromStringz(sval).idup;
+                }
+                if (FcPatternGetInteger(p, FC_INDEX, 0, &ival) == FcResultMatch)
+                {
+                    fr.faceIndex = ival;
+                }
+                res ~= fr;
+            }
+        }
+        return res;
+    }
+}
+
+
+private:
+
+int styleToFcSlant(in FontStyle style)
+{
+    final switch(style)
+    {
+        case FontStyle.normal:
+            return FC_SLANT_ROMAN;
+        case FontStyle.italic:
+            return FC_SLANT_ITALIC;
+        case FontStyle.oblique:
+            return FC_SLANT_OBLIQUE;
+    }
+}
+
+FontStyle fcSlantToStyle(in int slant)
+{
+    switch(slant)
+    {
+        case FC_SLANT_ROMAN:
+            return FontStyle.normal;
+        case FC_SLANT_ITALIC:
+            return FontStyle.italic;
+        case FC_SLANT_OBLIQUE:
+            return FontStyle.oblique;
+        default:
+            warningf("fontconfig slant %d do not match a FontStyle", slant);
+            return FontStyle.normal;
     }
 }
