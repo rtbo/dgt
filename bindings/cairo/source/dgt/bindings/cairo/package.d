ef5840d496f68cf0b797eca04ca0e657a26c1a62
Remi Thebault <remi.thebault@gmail.com>
2017-01-16 16:59:23 +0100

    cairo xcb/win32 imported only on linux/windows

--- bindings/cairo/source/dgt/bindings/cairo/package.d
+++ bindings/cairo/source/dgt/bindings/cairo/package.d
@@ -3,6 +3,12 @@ module dgt.bindings.cairo;
 public import dgt.bindings.cairo.enums;
 public import dgt.bindings.cairo.types;
 public import dgt.bindings.cairo.png;
-public import dgt.bindings.cairo.xcb;
-public import dgt.bindings.cairo.win32;
 public import dgt.bindings.cairo.symbols;
+version(linux)
+{
+    public import dgt.bindings.cairo.xcb;
+}
+version(Windows)
+{
+    public import dgt.bindings.cairo.win32;
+}
