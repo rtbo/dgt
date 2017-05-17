649b1ce5be9fced6f72014e2de9bdb73c432b515
Remi THEBAULT <remi.thebault@gmail.com>
2017-05-17 07:53:55 +0200

    parse css length

--- source/dgt/css/value.d
+++ source/dgt/css/value.d
@@ -1,6 +1,7 @@
 module dgt.css.value;
 
 import dgt.css.color;
+import dgt.css.token;
 
 struct Length
 {
@@ -10,10 +11,71 @@ struct Length
         // viewport relative
         vw, vh, vmin, vmax,
         // absolute
-        cm, mm, q, in_, pt, pc, px
+        cm, mm, q, inch, pt, pc, px
     }
     float val;
     Unit unit;
+}
+
+/// Parse a Length from a dimension token.
+/// Returns: true if parsing is successful, false otherwise.
+bool parseLength(Token tok, ref Length l)
+in {
+    assert(tok.tok == Tok.dimension);
+}
+body {
+    Length.Unit unit;
+    switch(tok.unit) {
+    case "em":
+        unit = Length.Unit.em;
+        break;
+    case "ex":
+        unit = Length.Unit.ex;
+        break;
+    case "ch":
+        unit = Length.Unit.ch;
+        break;
+    case "rem":
+        unit = Length.Unit.rem;
+        break;
+    case "vw":
+        unit = Length.Unit.vw;
+        break;
+    case "vh":
+        unit = Length.Unit.vh;
+        break;
+    case "vmin":
+        unit = Length.Unit.vmin;
+        break;
+    case "vmax":
+        unit = Length.Unit.vmax;
+        break;
+    case "cm":
+        unit = Length.Unit.cm;
+        break;
+    case "mm":
+        unit = Length.Unit.mm;
+        break;
+    case "q":
+        unit = Length.Unit.q;
+        break;
+    case "in":
+        unit = Length.Unit.inch;
+        break;
+    case "pt":
+        unit = Length.Unit.pt;
+        break;
+    case "pc":
+        unit = Length.Unit.pc;
+        break;
+    case "px":
+        unit = Length.Unit.px;
+        break;
+    default:
+        return false;
+    }
+    l = Length(cast(float)tok.num);
+    return true;
 }
 
 enum CSSWideValue
@@ -57,6 +119,11 @@ class CSSValue(T) : CSSValueBase
     this(T value)
     {
         _value = value;
+    }
+    this(Args...)(Args args)
+    if (is(typeof(T(args))))
+    {
+        _value = T(args);
     }
 
     final @property T value()
