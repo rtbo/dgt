833cd9511eed83399b09c14a9b4c923b6ded711c
Remi Thebault <remi.thebault@gmail.com>
2017-12-02 13:39:42 +0100

    enums

---
+++ source/dgt/core/enums.d
@@ -0,0 +1,69 @@
+/// Global enumerations.
+module dgt.core.enums;
+
+/// horizontal or vertical orientation
+enum Orientation
+{
+    horizontal,
+    vertical
+}
+
+/// ditto
+@property bool isHorizontal(in Orientation orientation) pure
+{
+    return orientation == Orientation.horizontal;
+}
+
+/// ditto
+@property bool isVertical(in Orientation orientation) pure
+{
+    return orientation == Orientation.vertical;
+}
+
+/// ditto
+@property Orientation other(in Orientation orientation) pure
+{
+    return orientation.isHorizontal ? Orientation.vertical : Orientation.horizontal;
+}
+
+
+enum Alignment
+{
+    none        = 0x00,
+
+    left        = 0x01,
+    right       = 0x02,
+    centerH     = 0x04,
+
+    top         = 0x10,
+    bottom      = 0x20,
+    centerV     = 0x40,
+
+    center      = centerH | centerV,
+}
+
+
+enum MouseButton
+{
+    none,
+
+    left,
+    middle,
+    right
+}
+
+enum MouseState
+{
+    none = 0,
+
+    left = 1,
+    middle = 1 << 1,
+    right = 1 << 2
+}
+
+enum FocusMethod
+{
+    program,
+    mouse,
+    keyboard
+}
