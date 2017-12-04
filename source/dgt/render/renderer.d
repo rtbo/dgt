16956cab917652cb4d65df7af23c83984aa0aca0
Remi Thebault <remi.thebault@gmail.com>
2017-12-04 08:24:13 +0100

    starting renderer

---
+++ source/dgt/render/renderer.d
@@ -0,0 +1,61 @@
+module dgt.render.renderer;
+
+import dgt.core.geometry;
+import dgt.render.framegraph;
+import gfx.device;
+import gfx.foundation.rc : Disposable, Rc;
+import gfx.pipeline;
+
+struct RenderOptions {
+    int samples;
+}
+
+class Renderer : Disposable {
+
+    this(Device device, RenderOptions options) {
+        _device = device;
+        _options = options;
+    }
+
+    void dispose() {
+        if (_cmdBuf.loaded) {
+            _rtv.unload();
+            _surf.unload();
+            _cmdBuf.unload();
+        }
+        _device.unload();
+    }
+
+    private void initialize() {
+        assert(!_cmdBuf);
+        _cmdBuf = _device.makeCommandBuffer();
+        // TODO pass actual window size
+        _surf = new BuiltinSurface!Rgba8(
+            _device.builtinSurface, 1, 1, cast(ubyte)_options.samples
+        );
+        _rtv = _surf.viewAsRenderTarget();
+    }
+
+    void renderFrame(immutable(FGFrame) frame) {
+        if (!_cmdBuf) {
+            initialize();
+        }
+
+        immutable vp = cast(Rect!ushort)frame.viewport;
+        auto encoder = Encoder(_cmdBuf);
+        encoder.setViewport(vp.x, vp.y, vp.width, vp.height);
+
+        import std.algorithm : each;
+        frame.clearColor.each!(
+            c => encoder.clear!Rgba8(_rtv, [c.r, c.g, c.b, c.a])
+        );
+
+        encoder.flush(_device);
+    }
+
+    private Rc!Device _device;
+    private RenderOptions _options;
+    private Rc!CommandBuffer _cmdBuf;
+    private Rc!(BuiltinSurface!Rgba8) _surf;
+    private Rc!(RenderTargetView!Rgba8) _rtv;
+}
