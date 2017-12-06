28d35c1a23617b01d13fca4c324a4b2a8054ca1d
Remi THEBAULT <remi.thebault@gmail.com>
2017-12-06 14:14:33 +0100

    hello example

---
+++ examples/hello/source/hello.d
@@ -0,0 +1,27 @@
+module hello;
+
+import dgt.application;
+import dgt.core.color : Color;
+import dgt.scene.scene : Scene;
+import dgt.window;
+
+import gfx.foundation.typecons : some;
+
+import std.math : PI;
+import std.stdio;
+import std.typecons : scoped;
+
+int main()
+{
+    auto app = new Application();
+    scope(exit) app.dispose();
+
+    auto win = new Window("Hello DGT");
+
+    auto sc = new Scene;
+    win.scene = sc;
+    sc.clearColor = some(Color.blue);
+
+    win.show();
+    return app.loop();
+}
