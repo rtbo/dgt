60cedf8adf31e5c03d0ab75ec58b9312bbb4f13d
Remi Thebault <remi.thebault@gmail.com>
2017-12-23 18:32:34 +0100

    demo of msdfgen in hello

--- examples/hello/source/hello.d
+++ examples/hello/source/hello.d
@@ -4,12 +4,17 @@ import core.time : dur;
 
 import dgt.application;
 import dgt.core.color : Color;
+import dgt.core.geometry;
+import dgt.core.image;
 import dgt.core.rc : rc;
+import dgt.font.msdfgen;
+import dgt.font.msdfgen.shape;
 import dgt.scene.scene : Scene;
 import dgt.window;
 
 import gfx.foundation.typecons : some;
 
+import std.exception;
 import std.math : PI;
 import std.stdio;
 import std.typecons : scoped;
@@ -37,10 +42,18 @@ int main()
     import dgt.font.style : FontStyle;
     auto fl = FontLibrary.create();
     auto fs = fl.matchFamily("serif").rc;
-    foreach(const i; 0 .. fs.styleCount) {
-        auto f = fs.createTypeface(i).rc;
-        writefln("%s %s", f.family, f.style);
-    }
+
+    auto tf = fs.createTypeface(0).rc;
+    auto shape = buildShape(tf, 1896);
+    enforce(shape.valid);
+    shape.normalize();
+
+    edgeColoringSimple(shape, 3f, 0);
+
+    auto data = new ubyte[32*32*4];
+    auto img = new Image(data, ImageFormat.xrgb, 32, 32*4);
+    generateMSDF(img, shape, 2, FVec2(1, 1), FVec2(0, 8), 1);
+    img.saveToFile("msdf.png");
 
     return app.loop();
 }
