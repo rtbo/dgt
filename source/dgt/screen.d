b1ba248c345d4b4be19aac54ae38de2c4db50ee0
Remi Thebault <remi.thebault@gmail.com>
2017-12-25 11:38:01 +0100

    getScreenDPI

--- source/dgt/screen.d
+++ source/dgt/screen.d
@@ -17,3 +17,30 @@ interface Screen
         return rect.height;
     }
 }
+
+/// Get the resolution in pixels per inch of a screen (screen 0 is the main monitor).
+/// If a platform is loaded (i.e. an Application instance is on), this will
+/// return the info from the loaded platform. Otherwise it instanciate a temporary
+/// platform to get the info from it.
+double getScreenDPI(int screen=0)
+{
+    import dgt.application : Application, makeDefaultPlatform;
+    import std.exception : enforce;
+
+    auto platform = Application.platform;
+    if (!platform) {
+        platform = makeDefaultPlatform();
+        platform.initialize();
+    }
+    enforce(platform);
+    scope(exit) {
+        if (!Application.platform) {
+            platform.dispose();
+        }
+    }
+
+    auto screens = platform.screens;
+    enforce(screen < screens.length);
+
+    return screens[screen].dpi;
+}
