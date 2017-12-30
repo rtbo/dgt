7beb2810bfc7e96bc9d29ad2e3c7c5bafeafbef8
Remi Thebault <remi.thebault@gmail.com>
2017-12-30 14:20:55 +0100

    hello to exemplify glyph runs

--- examples/hello/source/hello.d
+++ examples/hello/source/hello.d
@@ -7,9 +7,12 @@ import dgt.core.color : Color;
 import dgt.core.geometry;
 import dgt.core.image;
 import dgt.core.rc : rc;
-import dgt.font.msdfgen;
-import dgt.font.msdfgen.shape;
+import dgt.font.library;
+import dgt.font.style;
+import dgt.font.typeface;
 import dgt.ui : UserInterface;
+import dgt.ui.layout;
+import dgt.ui.text;
 import dgt.window;
 
 import gfx.foundation.typecons : some;
@@ -24,31 +27,40 @@ int main()
     auto app = new Application();
     scope(exit) app.dispose();
 
+    struct Proverb {
+        string text;
+        string css;
+    }
+    Proverb[] proverbs = [
+        Proverb("Assiduity makes all things easy.",             "font: 1in serif"),
+        Proverb("Le visage est le miroir du coeur.",            "font: italic 14px serif" ),
+        Proverb("Früh steh auf, wer ein Meister werden will",   "font: italic 1cm serif" ),
+        Proverb("لاتنفق كلمتين اذا كفتك كلمة ـ مثل عربي",        "font: 12px serif"),
+        // Proverb("花开堪折直需折",                                  "font: 12px serif" ),
+        // Proverb("あつささむさもひがんまで",                         "font: 12px serif" ),
+    ];
+
     auto ui = new UserInterface;
     ui.clearColor = some(Color.blue);
+    auto layout = new LinearLayout;
+    layout.setVertical();
+    foreach (p; proverbs) {
+        auto view = new TextView;
+        view.text = p.text;
+        view.inlineCSS = p.css;
+        layout.appendView(view);
+    }
+    ui.root = layout;
 
     auto win = new Window("Hello DGT");
     win.ui = ui;
     win.show();
 
     auto timer = Application.platform.createTimer();
-    timer.duration = dur!"seconds"(2);
+    timer.duration = dur!"seconds"(1);
     timer.handler = &win.close;
     timer.start();
     scope(exit) timer.dispose();
-
-    import dgt.font.library : FontLibrary;
-    auto fs = FontLibrary.get.matchFamily("serif").rc;
-
-    auto tf = fs.createTypeface(0).rc;
-    auto sc = tf.makeScalingContext(32).rc;
-
-    IVec2 bearing;
-    auto gl = sc.renderGlyph(957, bearing);
-    auto msdf = sc.renderGlyphMSDF(957, bearing);
-
-    gl.saveToFile("glyph.png");
-    msdf.saveToFile("msdf.png");
 
     return app.loop();
 }
