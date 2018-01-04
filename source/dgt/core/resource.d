e03974743f89a0c3a682fc641f2b37c6e7124fd1
Remi Thebault <remi.thebault@gmail.com>
2018-01-04 19:00:24 +0100

    retrieveResource

--- source/dgt/core/resource.d
+++ source/dgt/core/resource.d
@@ -3,21 +3,46 @@ module dgt.core.resource;
 
 import core.sync.mutex;
 
-/// Exception thrown by the registry when attempting to obtain a resource with
-/// a wrong identifier
+import std.regex;
+import std.uri;
+
+
+/// Exception thrown when attempting to obtain a resource with a wrong identifier.
 class UnavailableResourceException : Exception {
-    private this (string resName) {
+    private this (string uri) {
         import std.format : format;
-        super(format(`Resource "%s" is not available in the registry`, resName));
-        _resName = resName;
+        super(format(`Resource "%s" is not available`, uri));
+        _uri = uri;
     }
 
     /// the identifier that raised the exception
-    @property string resName() {
-        return _resName;
+    @property string uri() {
+        return _uri;
     }
 
-    private string _resName;
+    private string _uri;
+}
+
+/// Exception thrown when a resource is requested over an protocol that is not supported
+class UnsupportedProtocolException : Exception
+{
+    private this (string protocol, string uri) {
+        import std.format : format;
+        super(format(`Protocol "%s" is not supported during fetch of "%s"`, protocol, uri));
+    }
+
+    /// the unsupported protocol
+    @property string protocol() {
+        return _protocol;
+    }
+
+    /// the uri that was attempted to be fetched
+    @property string uri() {
+        return _uri;
+    }
+
+    private string _protocol;
+    private string _uri;
 }
 
 /// A binary blob that can be registered and retrieved in the registry
@@ -62,22 +87,77 @@ struct Registry {
         }
     }
 
-    /// Retrieve a resource from the registry
+    /// Retrieve a resource from the registry.
+    /// Throws:
+    ///     UnavailableResourceException if the resource is not found.
     static Resource get (string name) {
         mutex.lock();
         scope(exit) mutex.unlock();
 
         auto rp = name in registry;
         if (rp) return *rp;
-        else {
-            throw new UnavailableResourceException(name);
-        }
+
+        throw new UnavailableResourceException("dr://"~name);
     }
 }
+
+/// Retrieve a resource specified by uri.
+/// Different protocol are supported in the usual form of "protocol://resource".
+/// If no protocol is specified, "file://" is assumed.
+/// To obtain a value from `Registry`, "dr://" protocol should be used.
+/// Supported protocols:
+///     - dr://
+///     - file://
+/// If the URI is requested over a network, it is encoded beforehand, so uri
+/// passed to this function should not be encoded.
+Resource retrieveResource(in string uri)
+{
+    const regex = ctRegex!(uriRegex);
+    const m = matchFirst(uri, regex);
+
+    auto protocol = "file";
+    auto resId = uri;
+
+    if (m.length >= 3) {
+        protocol = m[1];
+        resId = m[2];
+    }
+
+    switch (protocol) {
+    case "file":
+        import std.exception : assumeUnique;
+        import std.file : exists, read;
+        if (!exists(resId)) {
+            throw new UnavailableResourceException(uri);
+        }
+        return assumeUnique(cast(ubyte[])read(resId));
+    case "dr":
+        return Registry.get(resId);
+    default:
+        break;
+    }
+
+    throw new UnsupportedProtocolException(protocol, uri);
+}
+
 
 private:
 
 Mutex mutex;
+
+enum uriRegex = `^(\w+)://(.+)`;
+
+unittest {
+    auto regex = ctRegex!(uriRegex);
+
+    string testUri = "http://this.uri/is.awesome";
+    auto m = matchFirst(testUri, regex);
+    assert(m);
+    assert(m.length == 3);
+    assert(m[0] == testUri);
+    assert(m[1] == "http");
+    assert(m[2] == "this.uri/is.awesome");
+}
 
 __gshared Resource[string] registry;
 
