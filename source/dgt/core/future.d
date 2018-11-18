106e85f3223f65f815d13e332c5f32803d34a172
Remi Thebault <remi.thebault@gmail.com>
2018-11-18 17:51:19 +0100

    update dgt.core.future

--- source/dgt/core/future.d
+++ source/dgt/core/future.d
@@ -1,40 +1,183 @@
 /// Provides Future/Promise synchronization
 module dgt.core.future;
 
+import std.traits : isCallable, Parameters, ReturnType;
+
+/// Status flag for Promise
 private enum Status : ubyte
 {
-    notStarted,
-    inProgress,
+    /// Value or Exception was not set
+    notDone,
+    /// Value or Exception was set
     done,
 }
 
-enum WaitPolicy {
+/// Wait policy for Promise
+enum WaitPolicy
+{
+    /// Wait is done by spinning on the CPU. Should be used for short time scale
+    /// tasks, i.e. shorter than a context switch.
     spin,
+    /// Wait by blocking on a condition variable
     block,
 }
 
+/// Future is an object that can resolve to a value or exception set in a
+/// different thread, or the same thread, possibly in a different frame
+/// (the value or exception are stored until resolve is called)
 struct Future(T)
 {
-    @disable this();
-    @disable this(this);
+    private shared(SharedState!T) state;
 
-    private shared(SharedState) state;
+    /// Check if it is valid to call resolve
+    @property bool valid()
+    {
+        return state && state.valid;
+    }
 
-    T resolve() {
+    /// Resolve the value or exception.
+    /// This can involve to wait for a task to finish in a different thread.
+    T resolve()
+    in (valid, "Future must be valid when resolve is called")
+    {
         return state.resolve();
     }
 }
 
-final class Task(WaitPolicy policy, alias fun, Args...)
+/// Spawns func in a different thread, passing it args, and return a Future
+/// object whose resolve method will wait for func completion and return its value
+/// or throw an exception that was not catched in func.
+/// Returns: A valid future to resolve the given func
+auto async(alias func, Args...)(Args args)
+if (is(typeof(func(args))))
 {
-    private enum spin = policy == WaitPolicy.spin;
-    private enum block = policy == WaitPolicy.block;
-    static assert(spin || block);
-
-
+    Task!func t;
+    auto f = t.future;
+    t.callAsync(args);
+    return f;
 }
 
-final class Promise(WaitPolicy policy, T)
+/// ditto
+auto async(F, Args...)(F func, Args args)
+if (is(typeof(func(args))))
+{
+    Task!(run, F) t = { callable : func };
+    auto f = t.future;
+    t.callAsync(args);
+    return f;
+}
+
+
+/// Build a Task that encapsulate func
+auto task(alias func)()
+{
+    Task!func t;
+    return t;
+}
+
+/// ditto
+auto task(F)(F func)
+if (isCallable!F)
+{
+    Task!(run, F) t = { callable : func };
+    return t;
+}
+
+
+/// Encapsulate a function whose result (return value or exception) is to be retrieved
+/// with a Future, possibly in a different thread than the thread executing the function.
+struct Task(alias func, F=void)
+if (is(F == void) || (isCallable!F && __traits(isSame, func, run)))
+{
+    static if (is(F == void)) {
+        private alias T = ReturnType!func;
+    }
+    else {
+        private alias T = ReturnType!F;
+        private F callable;
+    }
+
+    private alias SS = TaskSharedState!T;
+
+    private shared(SS) state;
+
+    /// Get the future for this task.
+    /// The future will be valid only when this task will start execution.
+    @property Future!T future()
+    {
+        if (!state) state = new SS;
+        return Future!T(state);
+    }
+
+    /// Spawn a thread into which the function is called with args as parameters
+    void callAsync(Args...)(Args args)
+    {
+        import core.thread : Thread;
+
+        if (!state) state = new SS;
+
+        static if (!is(F == void)) {
+            // push callable on the stack to make frame on the heap
+            // due to thread delegate
+            auto fp = callable;
+        }
+        synchronized(state) {
+            auto s = cast(SS)state;
+            s.valid = true;
+            s.th = new Thread(() {
+                static if (is(F == void)) {
+                    privCall(args);
+                }
+                else {
+                    privCall(fp, args);
+                }
+            });
+            s.th.start();
+        }
+    }
+
+    /// Call function with args as parameters. The returned value is stored in the
+    /// state shared with the Future
+    void call(Args...)(Args args)
+    {
+        if (!state) state = new SS;
+        auto s = cast(SS)state;
+        try {
+            s.valid = true;
+            static if (is(F == void)) {
+                privCall(args);
+            }
+            else {
+                privCall(callable, args);
+            }
+        }
+        catch(Throwable ex) {
+            s.ex = ex;
+        }
+    }
+
+    /// ditto
+    void opCall(Args...)(Args args)
+    {
+        call(args);
+    }
+
+    private void privCall(Args...)(Args args)
+    {
+        static if (is(T == void)) {
+            func(args);
+        }
+        else {
+            auto s = cast(SS)state;
+            s.val = func(args);
+        }
+    }
+}
+
+
+/// A Promise is represent the result of a task that is in a thread, and retrieved
+/// through a future in another thread.
+struct Promise(WaitPolicy policy, T)
 if (!is(T == void))
 {
     private enum spin = policy == WaitPolicy.spin;
@@ -44,18 +187,23 @@ if (!is(T == void))
     private alias SS = PromiseSharedState!(policy, T);
     private shared(SS) _sharedState;
 
-    this() {
-    }
-
+    /// Get the future of this promise.
+    /// The future is already valid. future.resolve will wait that the value or
+    /// exception is set in this promise.
     @property Future!T future()
     {
         if (!_sharedState) _sharedState = new SS;
         return Future!T(_sharedState);
     }
 
+    /// Set the value and fulfill this promise.
+    /// The future will be notified
     @property void value(T val)
     {
         import core.atomic : atomicLoad, atomicStore;
+
+        if (!_sharedState) _sharedState = new SS;
+
         assert(atomicLoad(_sharedState.status) != Status.done, "cannot set a promise value or exception twice");
 
         static if (spin) {
@@ -73,9 +221,13 @@ if (!is(T == void))
         }
     }
 
+    /// Set the exception that will be thrown by future.resolve.
     @property void exception(Throwable ex)
     {
         import core.atomic : atomicLoad, atomicStore;
+
+        if (!_sharedState) _sharedState = new SS;
+
         assert(atomicLoad(_sharedState.status) != Status.done, "cannot set a promise value or exception twice");
 
         static if (spin) {
@@ -95,11 +247,46 @@ if (!is(T == void))
 
 private:
 
+/// make task work with delegate
+ReturnType!F run(F, Args...)(F fpOrDelegate, ref Args args)
+{
+    return fpOrDelegate(args);
+}
+
 abstract class SharedState(T)
 {
+    private Throwable ex;
+    static if (!is(T == void)) {
+        private T val;
+    }
+
+    bool valid;
+
     shared abstract T resolve();
 }
 
+class TaskSharedState(T) : SharedState!T
+{
+    import core.thread : Thread;
+
+    private Thread th;
+
+    shared override T resolve()
+    {
+        synchronized(this) {
+            auto ss = cast(TaskSharedState!T)this;
+            if (ss.th) {
+                ss.th.join(true); // throws
+            }
+            else if (ss.ex) {
+                throw ss.ex;
+            }
+            static if (!is(T == void)) {
+                return ss.val;
+            }
+        }
+    }
+}
 
 class PromiseSharedState(WaitPolicy policy, T) : SharedState!T
 {
@@ -115,13 +302,12 @@ class PromiseSharedState(WaitPolicy policy, T) : SharedState!T
         private Condition condition;
     }
 
-    private Throwable ex;
-    private T val;
     private Status status;
 
-
-    static if (block) {
-        this() {
+    this()
+    {
+        valid = true;
+        static if (block) {
             mutex = new Mutex;
             condition = new Condition(mutex);
         }
