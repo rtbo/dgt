433494b3c0174b27944b2ab0f6ac8b9bda5b05ce
Remi Thebault <remi.thebault@gmail.com>
2018-12-02 23:23:24 +0100

    VgContext

---
+++ source/dgt/vg/context.d
@@ -0,0 +1,62 @@
+module dgt.vg.context;
+
+import dgt.core.color : Color;
+import dgt.core.image : Image;
+import dgt.core.paint : Paint;
+import dgt.vg.path : Path;
+import dgt.vg.penbrush : Brush, Pen;
+
+import gfx.core.rc : Disposable;
+import gfx.math : FMat2x3;
+
+interface VgContext : Disposable
+{
+    /// Get the image this context is drawing on
+    Image image();
+
+    /// Save and restore the context state.
+    /// The state include the following properties:
+    ///   - brush
+    ///   - pen
+    ///   - transform
+    ///   - clip
+    void save();
+    /// ditto
+    void restore();
+
+    /// Get/Set the transform of the context.
+    FMat2x3 transform() const;
+    /// ditto
+    void transform(const ref FMat2x3 transform);
+
+    /// Multiply the current transform by the given matrix
+    void pushTransform(const ref FMat2x3 transform);
+    /// Undo the last pushTransform operation.
+    /// The current transform is reset to the one before the last pushTransform
+    /// call, regardless if the transform was set by other means in between.
+    void popTransform();
+
+    /// Intersects the current clip path with path
+    void clip(immutable(Path) path);
+    /// Resets the clip region to the whole surface
+    void resetClip();
+
+    /// Mask the surface with the alpha plane of the image and paint it
+    /// with the current brush paint. img.format must be either ImageFormat.a1 or
+    /// ImageFormat.a8.
+    void mask(immutable(Image) img, immutable(Paint) paint);
+
+    /// Draw the image to the underlying image.
+    void drawImage(immutable(Image) img);
+
+    /// Clear the whole clipping area with the provided color.
+    /// This is equivalent has filling the clip path with a color Paint,
+    /// but can possibly be faster.
+    void clear(in Color color);
+
+    /// Stroke the given path using the given pen
+    void stroke(immutable(Path) path, immutable(Pen) pen=null);
+
+    /// Fill the given path using the given brush
+    void fill(immutable(Path) path, immutable(Brush) brush=null);
+}
